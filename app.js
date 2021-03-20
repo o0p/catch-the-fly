ae2fbc060342351972bf522fb97fa2e6461184b9
o0p <rus@gmx.com>
2021-03-20 17:45:41 +0000

    Add var selectors and default values vars

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const screen = document.querySelectorAll('.screen');
-
-screen[0].classList.add('up');
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
+const start_btn = document.getElementById('start-btn');
+const game_container = document.getElementById('game-container');
+const timeEl = document.getElementById('time');
+const scoreEl = document.getElementById('score');
+const messageEl = document.getElementById('message');
+let seconds = 0;
+let score = 0;
+let selected__insect = {};
