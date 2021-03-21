1d54de4ec851814715d8ace0a39909ddfecb3732
o0p <rus@gmx.com>
2021-03-21 13:49:59 +0000

    Add start_btn listener and screen transform

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const screen = document.querySelectorAll('.screen');
+const screens = document.querySelectorAll('.screen');
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
 const start_btn = document.getElementById('start-btn');
 const game_container = document.getElementById('game-container');
@@ -8,3 +8,5 @@ const messageEl = document.getElementById('message');
 let seconds = 0;
 let score = 0;
 let selected__insect = {};
+
+start_btn.addEventListener('click', () => screens[0].classList.add('up'));
