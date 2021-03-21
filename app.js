38b1c1e914d28cfee028ea21ed2630c2bd28e42f
o0p <rus@gmx.com>
2021-03-21 14:07:03 +0000

    Add createInsect() html injection

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const scoreEl = document.getElementById('score');
 const messageEl = document.getElementById('message');
 let seconds = 0;
 let score = 0;
-let selected__insect = {};
+let selected_insect = {};
 
 start_btn.addEventListener('click', () => screens[0].classList.add('up'));
 
@@ -16,9 +16,26 @@ choose_insect_btns.forEach(btn => {
         const img = btn.querySelector('img');
         const src = btn.getAttribute('src');
         const alt = btn.getAttribute('alt');
-        selected__insect = { src, alt };
+        selected_insect = { src, alt };
         screens[1].classList.add('up');
         setTimeout(createInsect, 1000);
         startGame();
     });
 });
+
+function createInsect() {
+    const insect = document.createElement('div');
+    insect.classList.add('insect');
+    const { x, y } = getRandomLocation();
+    insect.style.top = `${y}px`;
+    insect.style.left = `${x}px`;
+    insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}" />`
+};
+
+function getRandomLocation() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const x = Math.random() * (width - 200) + 100;
+    const y = Math.random() * (height - 200) + 100; 
+    return { x, y };
+}
